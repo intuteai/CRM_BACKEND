3a2eba6cf738068e2691ce91bfd76e5ecdf42a5b
rishikaratreja22 <rishikaratreja@gmail.com>
2025-12-02 12:17:05 +0530

    commits

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const pool = require('../config/db');
 
+
 const authenticateToken = async (req, res, next) => {
   const token = req.headers['authorization']?.split(' ')[1];
   if (!token) {
